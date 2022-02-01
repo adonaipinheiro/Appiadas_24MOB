fc75e17e603d319495810e72800cb2c7dc990537
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2022-01-31 21:56:12 -0300

    Aula 1

--- app/src/main/java/br/com/adonaipinheiro/appiadas/MainActivity.kt
+++ app/src/main/java/br/com/adonaipinheiro/appiadas/MainActivity.kt
@@ -1,11 +1,38 @@
 package br.com.adonaipinheiro.appiadas
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.adonaipinheiro.appiadas.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btTellJoker.setOnClickListener(){
+            showJoker()
+        }
     }
+
+    private fun showJoker() {
+        val jokers = resources.getStringArray(R.array.jokers)
+        val numberJoker = Random().nextInt(jokers.size)
+        val joker = jokers[numberJoker]
+        binding.tvJoker.text = joker
+        playSong()
+    }
+
+    private fun playSong() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.badumtss)
+        mediaPlayer.start()
+    }
+
 }
